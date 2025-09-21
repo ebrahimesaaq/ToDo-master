4ebff7489441d2917f7967321af10ddf47403f18
Ebrahim <ebrahimesaaq@gmail.com>
2025-09-21 13:24:11 +0300

    add routes

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:todo_master/core/consts/routes.dart';
 import 'package:todo_master/view/screen/auth/login.dart';
+import 'package:todo_master/view/screen/home_page.dart';
 
 Map<String, Widget Function(BuildContext)> myRoutes = {
   AppRoutes.login: (context) => const Login(),
+  AppRoutes.home: (context) => const HomePage(),
 };
